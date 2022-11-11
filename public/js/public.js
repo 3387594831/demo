47bee0f45c421908d35ec98ac42579d4464246df
3387594831 <114716528+3387594831@users.noreply.github.com>
2022-11-11 19:01:20 +0800

    Add files via upload
    Signed-off-by: 3387594831 <114716528+3387594831@users.noreply.github.com>

---
+++ public/js/public.js
@@ -0,0 +1,52 @@
+/* 小屏下侧边栏导航 */
+$('.samll-menuBox h3').next('ul').slideUp()
+$('.samll-menuBox  h3').click(function() {
+	$(this).next('ul').slideToggle().parent().siblings().children('ul').slideUp()
+
+	if (!$(this).children().hasClass('span-active')) {
+		$(this).children().addClass('span-active').parents('li').siblings().children('h3').children(
+			'span').removeClass('span-active')
+	} else {
+		$(this).children().removeClass('span-active')
+	}
+	//$(this).children().addClass('span-active')
+})
+$('.samll-menu').click(function() {
+	$('.samll-menuBox').show()
+	setTimeout(function() {
+		$('.samll-menuBox').css('background-color', 'rgba(0, 0, 0, 0.7)')
+	})
+	setTimeout(function() {
+		$('.samll-menuBox').children('ul').css({
+			'left': '0'
+		})
+	}, 200)
+})
+$('.samll-menuBox-close').click(function() {
+	$('.samll-menuBox').css('background-color', 'rgba(0, 0, 0, 0)')
+	setTimeout(function() {
+		$('.samll-menuBox').hide()
+	}, 600)
+	setTimeout(function() {
+		$('.samll-menuBox').children('ul').css({
+			'left': '-100%'
+		})
+	}, 200)
+})
+
+/* 搜索框 */
+$('.search-btn').click(function() {
+	$('.search-box').animate({
+		'top': 0
+	}, 800)
+})
+$('.searchbox-bottom').click(function() {
+	$('.search-box').animate({
+		'top': '-200%'
+	}, 800)
+})
+
+$('.f-r-bottom').click(function() {
+	$(this).children('ul').slideToggle()
+})
+$('.f-r-bottom').trigger('click')
